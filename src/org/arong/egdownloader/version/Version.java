cd35bef92aff2666c556c6be91c41c2544c8f129
dipoo <120826531@qq.com>
2014-06-03 09:23:39 +0800

    update to version 0.21

--- src/org/arong/egdownloader/version/Version.java
+++ src/org/arong/egdownloader/version/Version.java
@@ -8,10 +8,10 @@ public final class Version {
 	// 程序名
 	public final static String NAME = "EG下载器";
 	// 程序版本号
-	public final static String VERSION = "0.2";
+	public final static String VERSION = "0.21";
 	// 程序作者
 	public final static String AUTHOR = "阿荣";
 	// 程序最后修改时间
-	public final static String MODIFLIED = "2014-06-02";
+	public final static String MODIFLIED = "2014-06-03";
 	
 }
