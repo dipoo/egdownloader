287983f452a4737014352ce575e505417eda9c98
dipoo <120826531@qq.com>
2014-06-15 00:17:04 +0800

    图片表格单击将图片查看地址复制到剪贴板，设置表格为不可编辑状态。

--- src/org/arong/egdownloader/ui/table/PictureTable.java
+++ src/org/arong/egdownloader/ui/table/PictureTable.java
@@ -1,9 +1,13 @@
 package org.arong.egdownloader.ui.table;
 
 import java.awt.Color;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JTable;
 
+import org.arong.egdownloader.model.Picture;
 import org.arong.egdownloader.model.Task;
 import org.arong.egdownloader.ui.CursorManager;
 import org.arong.egdownloader.ui.window.EgDownloaderWindow;
@@ -32,6 +36,20 @@ public class PictureTable extends JTable {
 		
 		this.setModel(new PictureTableModel(task.getPictures()));
 		this.setDefaultRenderer(Object.class, new PictureTableCellRenderer());
+		this.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				PictureTable table = (PictureTable)e.getSource();
+				int rowIndex = table.rowAtPoint(e.getPoint());
+				Picture pic = table.getTask().getPictures().get(rowIndex);
+				StringSelection ss = new StringSelection(pic.getUrl());
+				table.getToolkit().getSystemClipboard().setContents(ss, ss);
+			}
+			
+		});
+	}
+
+	public boolean isCellEditable(int row, int column) {
+		return false;
 	}
 
 	public Task getTask() {
