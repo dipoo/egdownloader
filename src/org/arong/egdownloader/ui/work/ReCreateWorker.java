66d50f7f4fb2706c4599d14e153e1f3132ff066a
dipoo <120826531@qq.com>
2014-09-08 17:13:03 +0800

    任务重建

---
+++ src/org/arong/egdownloader/ui/work/ReCreateWorker.java
@@ -0,0 +1,97 @@
+package org.arong.egdownloader.ui.work;
+
+import java.io.InputStream;
+import java.net.SocketTimeoutException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+
+import org.apache.commons.httpclient.ConnectTimeoutException;
+import org.arong.egdownloader.model.ParseEngine;
+import org.arong.egdownloader.model.Setting;
+import org.arong.egdownloader.model.Task;
+import org.arong.egdownloader.model.TaskStatus;
+import org.arong.egdownloader.spider.SpiderException;
+import org.arong.egdownloader.spider.WebClient;
+import org.arong.egdownloader.spider.WebClientException;
+import org.arong.egdownloader.ui.table.TaskingTable;
+import org.arong.egdownloader.ui.window.CreatingWindow;
+import org.arong.egdownloader.ui.window.EgDownloaderWindow;
+import org.arong.util.FileUtil;
+/**
+ * 重新新建任务线程类
+ * @author 阿荣
+ * @since 2014-09-08
+ */
+public class ReCreateWorker extends SwingWorker<Void, Void>{
+	
+	private JFrame mainWindow;
+	private Task task;
+	public ReCreateWorker(Task task, JFrame mainWindow){
+		this.mainWindow = mainWindow;
+		this.task = task;
+	}
+	
+	protected Void doInBackground() throws Exception {
+		EgDownloaderWindow window = (EgDownloaderWindow)mainWindow;
+		if(window.creatingWindow == null){
+			window.creatingWindow = new CreatingWindow(mainWindow);
+		}
+		window.creatingWindow.setVisible(true);//显示新建任务详细信息窗口
+		Setting setting = window.setting;//获得配置信息
+		InputStream is;
+		try {
+			task = ParseEngine.buildTask_new(task, setting, window.creatingWindow);
+			if(task != null){
+				//下载封面
+				is =  WebClient.getStreamUseJava(task.getCoverUrl());
+				FileUtil.storeStream(task.getSaveDir(), "cover.jpg", is);//保存到目录
+				//保存到数据库
+				window.pictureDbTemplate.store(task.getPictures());//保存图片信息
+				window.taskDbTemplate.store(task);//保存任务
+				//保存到内存
+				TaskingTable taskTable = (TaskingTable)window.runningTable;
+				//关闭form,刷新table
+				((CreatingWindow)(window.creatingWindow)).reset();
+				window.creatingWindow.dispose();
+				window.tablePane.setVisible(true);//将表格panel显示出来
+				taskTable.updateUI();
+				window.setEnabled(true);
+				window.setVisible(true);
+				//下载
+				int maxThread = setting.getMaxThread();
+				TaskingTable table = (TaskingTable)window.runningTable;
+				if(table.getRunningNum() >= maxThread){
+					JOptionPane.showMessageDialog(null, "已达下载任务开启上限：" + maxThread);
+					return null;
+				}
+				task.setStatus(TaskStatus.STARTED);
+				task.setDownloadWorker(new DownloadWorker(task, table.getMainWindow()));
+				task.getDownloadWorker().execute();
+				table.setRunningNum(table.getRunningNum() + 1);
+			}else{
+				window.creatingWindow.dispose();
+				JOptionPane.showMessageDialog(null, "创建异常");
+			}
+		} catch (SocketTimeoutException e){
+			((CreatingWindow)(window.creatingWindow)).reset();
+			window.creatingWindow.dispose();
+			JOptionPane.showMessageDialog(null, "读取文件超时，请检查网络后重试");
+		} catch (ConnectTimeoutException e){
+			((CreatingWindow)(window.creatingWindow)).reset();
+			window.creatingWindow.dispose();
+			JOptionPane.showMessageDialog(null, "连接超时，请检查网络后重试");
+		} catch (SpiderException e) {
+			((CreatingWindow)(window.creatingWindow)).reset();
+			window.creatingWindow.dispose();
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		} catch (WebClientException e) {
+			((CreatingWindow)(window.creatingWindow)).reset();
+			window.creatingWindow.dispose();
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		return null;
+	}
+
+}
