da1d4fc0aebdb498ceee71ab99f67d1c2fdadbdf
dipoo <120826531@qq.com>
2015-01-09 11:37:02 +0800

    add popupMenu for textfield

--- src/org/arong/egdownloader/ui/swing/AJTextField.java
+++ src/org/arong/egdownloader/ui/swing/AJTextField.java
@@ -1,8 +1,21 @@
 package org.arong.egdownloader.ui.swing;
 
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
+
+import org.arong.egdownloader.ui.ComponentConst;
 /**
- * 封装JTextField,使构造函数可以设置name值,大小及坐标
+ * 封装JTextField,使构造函数可以设置name值,大小及坐标(含右键菜单)
  * @author 阿荣
  * @since 2013-8-25
  *
@@ -11,12 +24,128 @@ public class AJTextField extends JTextField {
 
 	private static final long serialVersionUID = -1763143133193131228L;
 	
+	private JPopupMenu popupMenu;
+	private Color menuItemColor = new Color(0,0,85);
+	//右键菜单：复制
+	private AJMenuItem copyMenuItem = new AJMenuItem("复制", menuItemColor, ComponentConst.SKIN_NUM + ComponentConst.SKIN_ICON.get("copy"), null);
+	//右键菜单：剪切
+	private AJMenuItem cutMenuItem = new AJMenuItem("剪切", menuItemColor, ComponentConst.SKIN_NUM + ComponentConst.SKIN_ICON.get("cut"), null);
+	//右键菜单：粘贴
+	private AJMenuItem pasteMenuItem = new AJMenuItem("粘贴", menuItemColor, ComponentConst.SKIN_NUM + ComponentConst.SKIN_ICON.get("paste"), null);
+	//右键菜单：清空
+	private AJMenuItem clearMenuItem = new AJMenuItem("清空", menuItemColor, ComponentConst.SKIN_NUM + ComponentConst.SKIN_ICON.get("clear"), null);
+	//右键菜单：全选
+	private AJMenuItem selectAllMenuItem = new AJMenuItem("全选", menuItemColor, null);
+	
 	public AJTextField(String name, int x, int y, int width, int height){
 		this.setName(name);
 		this.setBounds(x, y, width, height);
+		final AJTextField this_ = this;
+		
+		copyMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selectText = this_.getSelectedText();
+				if(selectText != null && !"".equals(selectText)){
+					StringSelection ss = new StringSelection(selectText);
+					Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, ss);
+				}
+			}
+		});
+		
+		cutMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selectText = this_.getSelectedText();
+				if(selectText != null && !"".equals(selectText)){
+					StringSelection ss = new StringSelection(selectText);
+					Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, ss);
+					this_.setText(this_.getText().replaceAll(selectText, ""));
+				}
+			}
+		});
+		
+		pasteMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Transferable t = this_.getToolkit().getSystemClipboard().getContents(this);
+				//判断内容是否为空，是否为字符串
+				if(t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)){
+					try {
+						this_.setText(t.getTransferData(DataFlavor.stringFlavor) + "");
+					} catch (Exception e1) {
+						System.out.println("粘贴出错");
+					}
+				}
+			}
+		});
+		
+		clearMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				this_.setText("");
+			}
+		});
+		
+		selectAllMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				this_.selectAll();
+			}
+		});
+		
+		popupMenu = new AJPopupMenu(copyMenuItem, cutMenuItem, pasteMenuItem, clearMenuItem, selectAllMenuItem);
+		this.addMouseListener(new MouseListener() {
+			public void mouseReleased(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseClicked(MouseEvent e) {
+				AJTextField field = (AJTextField)e.getSource();
+				//获取点击的行数
+				if(e.getButton() == MouseEvent.BUTTON3){
+					String text = this_.getText();
+					String selectText = this_.getSelectedText();
+					//是否复制
+					if(selectText == null || "".equals(selectText)){
+						copyMenuItem.setEnabled(false);
+						copyMenuItem.setForeground(Color.GRAY);
+						cutMenuItem.setEnabled(false);
+						cutMenuItem.setForeground(Color.GRAY);
+					}else{
+						copyMenuItem.setEnabled(true);
+						copyMenuItem.setForeground(menuItemColor);
+						cutMenuItem.setEnabled(true);
+						cutMenuItem.setForeground(menuItemColor);
+					}
+					
+					//是否粘贴
+					Transferable t = this_.getToolkit().getSystemClipboard().getContents(this);
+					//判断内容是否为空，是否为字符串
+					if(t == null || !t.isDataFlavorSupported(DataFlavor.stringFlavor)){
+						pasteMenuItem.setEnabled(false);
+						pasteMenuItem.setForeground(Color.GRAY);
+					}else{
+						pasteMenuItem.setEnabled(true);
+						pasteMenuItem.setForeground(menuItemColor);
+					}
+					//是否清空/全选
+					if("".equals(text)){
+						clearMenuItem.setEnabled(false);
+						clearMenuItem.setForeground(Color.GRAY);
+						selectAllMenuItem.setEnabled(false);
+						selectAllMenuItem.setForeground(Color.GRAY);
+					}else{
+						clearMenuItem.setEnabled(true);
+						clearMenuItem.setForeground(menuItemColor);
+						selectAllMenuItem.setEnabled(true);
+						selectAllMenuItem.setForeground(menuItemColor);
+					}
+					
+					popupMenu.show(field, e.getPoint().x, e.getPoint().y);
+				}
+			}
+		});
+		
 	}
 	public AJTextField(String text,String name, int x, int y, int width, int height){
 		this(name, x, y, width, height);
 		this.setText(text);
 	}
+	
 }
