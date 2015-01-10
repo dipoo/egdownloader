dd244df7249e4988fa7a4d37246ee9406a970bb6
dipoo <120826531@qq.com>
2015-01-10 13:30:40 +0800

    add delete popupmenu for grouplist

--- src/org/arong/egdownloader/ui/list/GroupList.java
+++ src/org/arong/egdownloader/ui/list/GroupList.java
@@ -1,21 +1,30 @@
 package org.arong.egdownloader.ui.list;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 import org.arong.egdownloader.db.impl.PictureDom4jDbTemplate;
 import org.arong.egdownloader.db.impl.SettingDom4jDbTemplate;
 import org.arong.egdownloader.db.impl.TaskDom4jDbTemplate;
 import org.arong.egdownloader.ui.ComponentConst;
 import org.arong.egdownloader.ui.CursorManager;
+import org.arong.egdownloader.ui.swing.AJMenuItem;
+import org.arong.egdownloader.ui.swing.AJPopupMenu;
 import org.arong.egdownloader.ui.window.EgDownloaderWindow;
 import org.arong.egdownloader.ui.window.GroupWindow;
 import org.arong.egdownloader.ui.window.InitWindow;
+import org.arong.util.FileUtil;
 /**
  * 任务组列表
  * @author dipoo
@@ -24,11 +33,16 @@ import org.arong.egdownloader.ui.window.InitWindow;
 public class GroupList extends JList {
 
 	private static final long serialVersionUID = -7702879865264332528L;
+	
+	private JPopupMenu popupMenu;
+	private JMenuItem deleteMenu = new AJMenuItem("删除", new Color(0,0,85), ComponentConst.SKIN_NUM + ComponentConst.SKIN_ICON.get("delete"), null);
+	
 	public GroupList(List<File> groups, final GroupWindow window, final EgDownloaderWindow mainWindow){
 		this.setModel(new GroupListModel(groups));
 		//this.setCellRenderer(new GroupListCellReader());
 		this.setCursor(CursorManager.getPointerCursor());
 		this.setForeground(Color.BLUE);
+		
 		this.addMouseListener(new MouseListener() {
 			public void mouseReleased(MouseEvent arg0) {}
 			public void mousePressed(MouseEvent arg0) {}
@@ -36,39 +50,120 @@ public class GroupList extends JList {
 			public void mouseEntered(MouseEvent arg0) {}
 			public void mouseClicked(MouseEvent me) {
 				int clickNum = me.getClickCount();
-				//双击
-				if(2 == clickNum){
-					GroupList list = (GroupList)me.getSource();
-					String groupName = "";
-					if(list.getSelectedIndex() != 0){
-						groupName = list.getSelectedValue().toString();
-						if(groupName.indexOf("|") != -1){
-							groupName = groupName.substring(0, groupName.indexOf("|"));
+				GroupList list = (GroupList)me.getSource();
+				//左键
+				if(me.getButton() == MouseEvent.BUTTON1){
+					//双击
+					if(2 == clickNum){
+						String groupName = "";
+						if(list.getSelectedIndex() != 0){
+							groupName = list.getSelectedValue().toString();
+							if(groupName.indexOf("|") != -1){
+								groupName = groupName.substring(0, groupName.indexOf("|"));
+							}
 						}
-					}
-					//如果是在主界面切换的任务组，且选择的任务组不变，则关闭任务组列表窗口
-					if(mainWindow != null && groupName.equals(ComponentConst.groupName)){
+						//如果是在主界面切换的任务组，且选择的任务组不变，则关闭任务组列表窗口
+						if(mainWindow != null && groupName.equals(ComponentConst.groupName)){
+							window.dispose();
+							return;
+						}
+						if(mainWindow != null){
+							//保存前一个任务组的数据
+							mainWindow.saveTaskGroupData();
+							mainWindow.dispose();
+						}
+						ComponentConst.groupName = groupName;
+						ComponentConst.changeDataPath(groupName);
+						ComponentConst.changeDataXmlPath();
 						window.dispose();
-						return;
+						if(mainWindow != null){
+							/**
+							 * 更新dom
+							 */
+							SettingDom4jDbTemplate.updateDom();
+							TaskDom4jDbTemplate.updateDom();
+							PictureDom4jDbTemplate.updateDom();
+						}
+						new InitWindow();
 					}
-					if(mainWindow != null){
-						//保存前一个任务组的数据
-						mainWindow.saveTaskGroupData();
-						mainWindow.dispose();
+				}//右键
+				else if(me.getButton() == MouseEvent.BUTTON3){
+					int rowIndex = list.locationToIndex(me.getPoint());
+					//使之选中
+					list.getSelectionModel().setSelectionInterval(rowIndex, rowIndex);
+					if(popupMenu == null){
+						popupMenu = new AJPopupMenu(deleteMenu);
 					}
-					ComponentConst.groupName = groupName;
-					ComponentConst.changeDataPath(groupName);
-					ComponentConst.changeDataXmlPath();
-					window.dispose();
-					if(mainWindow != null){
-						/**
-						 * 更新dom
-						 */
-						SettingDom4jDbTemplate.updateDom();
-						TaskDom4jDbTemplate.updateDom();
-						PictureDom4jDbTemplate.updateDom();
+					popupMenu.show(list, me.getPoint().x, me.getPoint().y);
+				}
+				
+			}
+		});
+		final GroupList this_ = this;
+		deleteMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(this_.getSelectedIndex() == 0){
+					JOptionPane.showMessageDialog(this_, "默认空间不能删除");
+				}else{
+					String name = this_.getSelectedValue().toString();
+					if(name.indexOf("|") != -1){
+						name = name.substring(0, name.indexOf("|"));
 					}
-					new InitWindow();
+					int r = JOptionPane.showConfirmDialog(this_, "删除任务组将会删除已下载的漫画，且不可恢复，您确定要删除任务组：" + name + "吗？");
+					if(r == JOptionPane.OK_OPTION){
+						//如果是在主界面切换的任务组，且选择的任务组不变，则关闭主窗口
+						if(mainWindow != null && name.equals(ComponentConst.groupName)){
+							mainWindow.dispose();
+						}
+						File file = new File(ComponentConst.ROOT_DATA_PATH + "/" + name);
+						if(file.exists()){
+							FileUtil.deleteFile(file);
+						}
+						File dataFile = new File(ComponentConst.ROOT_DATA_PATH);
+						if(!dataFile.exists()){
+							dataFile.mkdirs();
+							if(window != null){
+								window.dispose();
+							}
+							ComponentConst.groupName = "";
+							ComponentConst.changeDataPath("");
+							ComponentConst.changeDataXmlPath();
+							/**
+							 * 更新dom
+							 */
+							SettingDom4jDbTemplate.updateDom();
+							TaskDom4jDbTemplate.updateDom();
+							PictureDom4jDbTemplate.updateDom();
+							new InitWindow();
+						}else{
+							File[] files = dataFile.listFiles();
+							List<File> groups = new ArrayList<File>();
+							for(File file2 : files){
+								if(file2.isDirectory()){
+									groups.add(file2);
+								}
+							}
+							if(groups.size() > 0){
+								this_.setModel(new GroupListModel(groups));
+								this_.updateUI();
+							}else{
+								if(window != null){
+									window.dispose();
+								}
+								ComponentConst.groupName = "";
+								ComponentConst.changeDataPath("");
+								ComponentConst.changeDataXmlPath();
+								/**
+								 * 更新dom
+								 */
+								SettingDom4jDbTemplate.updateDom();
+								TaskDom4jDbTemplate.updateDom();
+								PictureDom4jDbTemplate.updateDom();
+								new InitWindow();
+							}
+						}
+						
+					}
 				}
 			}
 		});
--- src/org/arong/egdownloader/ui/work/DeleteWorker.java
+++ src/org/arong/egdownloader/ui/work/DeleteWorker.java
@@ -13,6 +13,7 @@ import org.arong.egdownloader.ui.ComponentConst;
 import org.arong.egdownloader.ui.table.TaskingTable;
 import org.arong.egdownloader.ui.window.DeletingWindow;
 import org.arong.egdownloader.ui.window.EgDownloaderWindow;
+import org.arong.util.FileUtil;
 /**
  * 删除任务线程类
  * @author 阿荣
@@ -63,7 +64,7 @@ public class DeleteWorker extends SwingWorker<Void, Void>{
 					for(int i = 0; i < tasks.size(); i ++){
 						file = new File(ComponentConst.getSavePathPreffix() + tasks.get(i).getSaveDir());
 						if(file.exists()){
-							this.deleteFile(file);
+							FileUtil.deleteFile(file);
 						}
 					}
 				}
@@ -84,18 +85,5 @@ public class DeleteWorker extends SwingWorker<Void, Void>{
 		}	
 		return null;
 	}
-
-	private void deleteFile(File file){ 
-		if(file.exists()){                    //判断文件是否存在
-		   if(file.isFile()){                    //判断是否是文件
-		     file.delete();                       //delete()方法 你应该知道 是删除的意思;
-		   }else if(file.isDirectory()){              //否则如果它是一个目录
-		     File files[] = file.listFiles();               //声明目录下所有的文件 files[];
-		     for(int i=0;i<files.length;i++){            //遍历目录下所有的文件
-		      this.deleteFile(files[i]);             //把每个文件 用这个方法进行迭代
-		     } 
-		   } 
-		    file.delete(); 
-		} 
-	} 
+	 
 }
