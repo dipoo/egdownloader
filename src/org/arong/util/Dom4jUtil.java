eee437b6b9599fc8c18ca25f70ebbcc520e61d76
dipoo <120826531@qq.com>
2015-01-10 11:23:36 +0800

    makedir before save data

--- src/org/arong/util/Dom4jUtil.java
+++ src/org/arong/util/Dom4jUtil.java
@@ -40,7 +40,9 @@ public final class Dom4jUtil {
     }
     
     public static void writeDOM2XML(String file_path, Document doc) throws Exception{
-       writeDOM2XML(new File(file_path), doc);
+    	System.out.println(file_path.substring(0, file_path.lastIndexOf("/")));
+    	FileUtil.ifNotExistsThenCreate(file_path.substring(0, file_path.lastIndexOf("/")));
+        writeDOM2XML(new File(file_path), doc);
     }
 
     public static void writeDOM2XML(File file, Document doc) throws Exception{
