37ac4aee4e7ab4415f9588099d17b999936f3d75
dipoo <120826531@qq.com>
2019-04-24 15:32:37 +0800

    优化代理测试

--- src/org/arong/egdownloader/ui/window/TestProxyWindow.java
+++ src/org/arong/egdownloader/ui/window/TestProxyWindow.java
@@ -1,11 +1,12 @@
 package org.arong.egdownloader.ui.window;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Window;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -15,19 +16,24 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.EditorKit;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.html.HTMLEditorKit;
 
+import org.apache.commons.lang.StringUtils;
 import org.arong.egdownloader.model.Setting;
+import org.arong.egdownloader.spider.Spider;
 import org.arong.egdownloader.spider.WebClient;
 import org.arong.egdownloader.ui.ComponentUtil;
 import org.arong.egdownloader.ui.listener.MouseAction;
 import org.arong.egdownloader.ui.listener.OperaBtnMouseListener;
 import org.arong.egdownloader.ui.swing.AJButton;
 import org.arong.egdownloader.ui.swing.AJLabel;
-import org.arong.egdownloader.ui.swing.AJTextArea;
 import org.arong.egdownloader.ui.swing.AJTextField;
+import org.arong.egdownloader.ui.work.CommonSwingWorker;
 import org.arong.egdownloader.ui.work.interfaces.IListenerTask;
 /**
  * 代理测试窗口
@@ -45,7 +51,7 @@ public class TestProxyWindow extends JDialog{
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		JLabel urlLabel = new AJLabel("测试地址:", Color.BLUE, 10, 15, 60, 30);
-		final JTextField urlField = new AJTextField("http://ip.catr.cn/", "", 70, 15, 435, 30);
+		final JTextField urlField = new AJTextField("http://ip.catr.cn/", "", 70, 15, 435, 30);//http://www.ip111.cn/
 		JLabel typeLabel = new AJLabel("编码类型:", Color.BLUE, 10, 55, 60, 30);
 		ButtonGroup buttonGroup = new ButtonGroup();
 		final JRadioButton rb1 = new JRadioButton("UTF-8", true);
@@ -54,46 +60,74 @@ public class TestProxyWindow extends JDialog{
 		rb2.setBounds(200, 55, 100, 30);
 		buttonGroup.add(rb1);
 		buttonGroup.add(rb2);
-		final JTextArea resultArea = new AJTextArea();
-		resultArea.setEditable(false);
+		final AJLabel spendtime = new AJLabel("", Color.BLUE);
+		spendtime.setBounds(350, 55, 200, 30);
+		final JTextPane resultArea = new JTextPane();
+		final EditorKit editorKit = new HTMLEditorKit();
+		final EditorKit noeditorKit = new StyledEditorKit();
+		//resultArea.setEditable(false);
 		resultArea.setAutoscrolls(true);
-		resultArea.setLineWrap(true);
 		resultArea.setBorder(null);
-		resultArea.setFont(new Font("宋体", Font.PLAIN, 12));
-		resultArea.setForeground(new Color(63,127,95));
 		
-		JScrollPane consolePane = new JScrollPane(resultArea);
+		JScrollPane consolePane = new JScrollPane();
 		TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(219,219,219)), "测试结果");
 		consolePane.setBounds(10, 100, 568, 310);
 		consolePane.setAutoscrolls(true);
 		consolePane.setBorder(border);
+		consolePane.setViewportView(resultArea);
 		
 		final JButton testBtn = new AJButton("执行测试", "", "", new OperaBtnMouseListener(this, MouseAction.CLICK, new IListenerTask() {
 			public void doWork(Window window, MouseEvent e) {
-				String url = urlField.getText();
-				if("".equals(url)){
-					JOptionPane.showMessageDialog(null, "地址不能为空");
-					return;
-				}
-				//resultArea.setText("");
-				String result = null;
-				try{
-//					if(rb1.isSelected()){
-						result = WebClient.getRequestUseJava(url, rb1.isSelected() ? rb1.getText() : rb2.getText());
-//					}
-					/*if(rb2.isSelected()){
-						result = WebClient.postRequest(url, "gb2312");
-					}*/
-					resultArea.setText(result);
-				}catch(Exception e1){
-					e1.printStackTrace();
-					resultArea.setText(e1.getMessage());
-				}
+				final JButton source = (JButton) e.getSource();
+				new CommonSwingWorker(new Runnable() {
+					public void run() {
+						source.setEnabled(false);
+						long t = System.currentTimeMillis();
+						String url = urlField.getText();
+						if("".equals(url)){
+							JOptionPane.showMessageDialog(null, "地址不能为空");
+							return;
+						}
+						String result = null;
+						try{
+							result = WebClient.getRequestUseJavaWithCookie(url, rb1.isSelected() ? rb1.getText() : rb2.getText(), null, 10 * 1000); 
+							if(StringUtils.isNotBlank(result)){
+								boolean html = true;String source = "";
+								if(result.toLowerCase().contains("<body>")){
+									source = Spider.getTextFromSource(result.toLowerCase(), "<body>", "</body>");
+									source = filterImg(source);
+								}else if(result.toLowerCase().contains("<body")){
+									source = Spider.getTextFromSource(result.toLowerCase(), "<body", "</body>");
+									source = Spider.substring(source, ">");
+									source = filterImg(source);
+								}else{
+									html = false;
+								}
+								
+								if(html){
+									resultArea.setEditorKit(editorKit);
+								}else{
+									resultArea.setEditorKit(noeditorKit);
+								}
+								resultArea.setText(source);
+							}else{
+								resultArea.setText("[空]");
+							}
+						}catch(Exception e1){
+							e1.printStackTrace();
+							resultArea.setText(e1.getMessage());
+						}finally{
+							spendtime.setText("耗时：" + (System.currentTimeMillis() - t) + "ms");
+							source.setEnabled(true);
+							resultArea.select(resultArea.getDocument().getLength(), resultArea.getDocument().getLength());
+						}
+					}
+				}).execute();
 			}
 		}), 515, 15, 60, 30);
 		
 		
-		ComponentUtil.addComponents(getContentPane(), urlLabel, urlField, testBtn, typeLabel, rb1, rb2, consolePane);
+		ComponentUtil.addComponents(getContentPane(), urlLabel, urlField, testBtn, typeLabel, rb1, rb2, spendtime, consolePane);
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				//关闭后显示主界面
@@ -109,5 +143,13 @@ public class TestProxyWindow extends JDialog{
 		});
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setVisible(true);
+	}
+	
+	public String filterImg(String str) {
+		String reg = "<img[^>]*>";
+		Pattern pat = Pattern.compile(reg);
+		Matcher mat = pat.matcher(str);
+		String s = mat.replaceAll("").trim();
+		return s;
 	}
 }
